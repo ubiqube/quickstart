d726e403b673ba46a24616ca81a08e42390ee914
antoine-brun <antoine.brun@gmail.com>
2020-06-29 23:26:36 +0200

    restart tomcat

--- lab/api/resources/install_libraries.sh
+++ lab/api/resources/install_libraries.sh
@@ -126,6 +126,7 @@ chown -R ncuser:ncuser /opt/ubi-jentreprise/resources/templates/conf/device;\
 /opt/ubi-jentreprise/configure; 
 service wildfly restart; 
 service wildfly status; 
+/opt/ses/configure
 service tomcat restart; 
 
 /opt/sms/configure;
