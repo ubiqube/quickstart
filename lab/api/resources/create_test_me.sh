6425866df98b231ac113c19a05926a9ff55231ee
antoine-brun <antoine.brun@gmail.com>
2020-07-01 17:28:41 +0200

    add checkpoint

--- lab/api/resources/create_test_me.sh
+++ lab/api/resources/create_test_me.sh
@@ -48,6 +48,13 @@ echo "--------------------------------------------------"
 echo "CREATE DEMO DEVICES"
 echo "--------------------------------------------------"
 CUSTIDONLY=${CUSTID//BLRA}
+
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/orchestration/service/execute/$CUSTID/?serviceName=Process/SelfDemoSetup/SelfDemoSetup&processName=Process%2FSelfDemoSetup%2FProcess_Setup" \
-    -d '{"managed_device_name":"PFSENSE", "manufacturer_id":"200425", "model_id":"200425"}'
+    -d '{  "manufacturer_id": "200425",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "managed_device_name": "pfsense",  "model_id": "200425",  "device_ip_address": "192.168.1.1",  "login": "msa", }'
+
+curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/orchestration/service/execute/$CUSTID/?serviceName=Process/SelfDemoSetup/SelfDemoSetup&processName=Process%2FSelfDemoSetup%2FProcess_Setup" \
+    -d '{  "manufacturer_id": "18082900",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "managed_device_name": "checkpoint_R80",  "model_id": "18082900",  "device_ip_address": "192.168.1.1",  "login": "msa", }'
+
+
+
 echo
