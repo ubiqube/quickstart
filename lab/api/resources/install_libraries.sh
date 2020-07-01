ff55301b0e96c6fa48dda2b84832883098e8ef39
antoine-brun <antoine.brun@gmail.com>
2020-07-01 13:32:39 +0200

    install other DA

--- lab/api/resources/install_libraries.sh
+++ lab/api/resources/install_libraries.sh
@@ -128,14 +128,14 @@ update_github_repo
 
 
 install_adapter linux_generic 
-install_adapter oneaccess_lbb conf_netconf USER_MODE
-install_adapter oneaccess_lbb conf_generic USER_MODE
-install_adapter oneaccess_lbb conf_whitebox USER_MODE
+#install_adapter oneaccess_lbb conf_netconf USER_MODE
+#install_adapter oneaccess_lbb conf_generic USER_MODE
+#install_adapter oneaccess_lbb conf_whitebox USER_MODE
 #install_adapter stormshield 
-#install_adapter adva_nc 
-#install_adapter f5_bigip 
+install_adapter adva_nc 
+install_adapter f5_bigip 
 #install_adapter a10_thunder 
-#install_adapter virtuora_nc 
+install_adapter virtuora_nc 
 
 install_microservices;
 install_workflows;
