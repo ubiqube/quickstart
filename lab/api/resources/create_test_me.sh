5343f29d8db65f8d239efb1cafc3e6e486a503e3
antoine-brun <antoine.brun@gmail.com>
2020-07-03 15:19:12 +0200

    typo

--- lab/api/resources/create_test_me.sh
+++ lab/api/resources/create_test_me.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 #set -x
+
+######################################################################################
+# This script is only meant to be used as a test script to create a ME for each vendor
+######################################################################################
+
 USER="ncroot"
 PASSWORD="ubiqube"
 OPERATOR="TST"
@@ -15,7 +20,7 @@ RESPONSE=`curl -s -H 'Content-Type: application/json' -XPOST http://127.0.0.1/ub
 TOKEN=$(php -r 'echo json_decode($argv[1])->token;' "$RESPONSE")
 
 echo "-------------------------------------------------------"
-echo "CREATE $OPERATOR TENANT AND CUSTOMER Tyrell Corporation"
+echo "CREATE $OPERATOR TENANT AND CUSTOMER my_subtenant"
 echo "-------------------------------------------------------"
 
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/operator/$OPERATOR?name=$OPERATOR_NAME"
@@ -51,43 +56,43 @@ CUSTIDONLY=${CUSTID//BLRA}
 
 echo "### pfSense"
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/orchestration/service/execute/status/$CUSTID/?serviceName=Process/SelfDemoSetup/SelfDemoSetup&processName=Process%2FSelfDemoSetup%2FCreate_ME" \
-    -d '{  "manufacturer_id": "200425",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "managed_device_name": "pfsense",  "model_id": "200425",  "device_ip_address": "192.168.1.1",  "login": "msa", }'
+    -d '{  "manufacturer_id": "200425",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "managed_device_name": "pfsense",  "model_id": "200425",  "device_ip_address": "192.168.1.1",  "login": "msa" }'
 
 echo "#### Checkpoint"
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/orchestration/service/execute/status/$CUSTID/?serviceName=Process/SelfDemoSetup/SelfDemoSetup&processName=Process%2FSelfDemoSetup%2FCreate_ME" \
-    -d '{  "manufacturer_id": "18082900",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "managed_device_name": "checkpoint_R80",  "model_id": "18082900",  "device_ip_address": "192.168.1.1",  "login": "msa", }'
+    -d '{  "manufacturer_id": "18082900",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "managed_device_name": "checkpoint_R80",  "model_id": "18082900",  "device_ip_address": "192.168.1.1",  "login": "msa" }'
 
 echo "#### REST Generic"
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/orchestration/service/execute/status/$CUSTID/?serviceName=Process/SelfDemoSetup/SelfDemoSetup&processName=Process%2FSelfDemoSetup%2FCreate_ME" \
-    -d '{  "manufacturer_id": "191119",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "managed_device_name": "REST_generic",  "model_id": "191119",  "device_ip_address": "192.168.1.1",  "login": "msa", }'
+    -d '{  "manufacturer_id": "191119",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "managed_device_name": "REST_generic",  "model_id": "191119",  "device_ip_address": "192.168.1.1",  "login": "msa" }'
 
 echo "#### AWS Generic"
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/orchestration/service/execute/status/$CUSTID/?serviceName=Process/SelfDemoSetup/SelfDemoSetup&processName=Process%2FSelfDemoSetup%2FCreate_ME" \
-    -d '{ "managed_device_name": "AWS_gen",  "model_id": "17010301", "manufacturer_id": "17010301",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa", }'
+    -d '{ "managed_device_name": "AWS_gen",  "model_id": "17010301", "manufacturer_id": "17010301",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa" }'
 
 echo "#### ADVA NC"
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/orchestration/service/execute/status/$CUSTID/?serviceName=Process/SelfDemoSetup/SelfDemoSetup&processName=Process%2FSelfDemoSetup%2FCreate_ME" \
-    -d '{ "managed_device_name": "ADVA NC",  "model_id": "18100200", "manufacturer_id": "18100200",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa", }'
+    -d '{ "managed_device_name": "ADVA NC",  "model_id": "18100200", "manufacturer_id": "18100200",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa" }'
 
 echo "#### F5_BIGIP"
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/orchestration/service/execute/status/$CUSTID/?serviceName=Process/SelfDemoSetup/SelfDemoSetup&processName=Process%2FSelfDemoSetup%2FCreate_ME" \
-    -d '{ "managed_device_name": "F5_BIGIP",  "model_id": "50001", "manufacturer_id": "50000",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa", }'
+    -d '{ "managed_device_name": "F5_BIGIP",  "model_id": "50001", "manufacturer_id": "50000",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa"}'
 
 echo "#### Virtuora NC"
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/orchestration/service/execute/status/$CUSTID/?serviceName=Process/SelfDemoSetup/SelfDemoSetup&processName=Process%2FSelfDemoSetup%2FCreate_ME" \
-    -d '{ "managed_device_name": "Virtuora_NC",  "model_id": "18100100", "manufacturer_id": "18100100",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa", }'
+    -d '{ "managed_device_name": "Virtuora_NC",  "model_id": "18100100", "manufacturer_id": "18100100",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa" }'
 
 echo "#### Cisco WSA"
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/orchestration/service/execute/status/$CUSTID/?serviceName=Process/SelfDemoSetup/SelfDemoSetup&processName=Process%2FSelfDemoSetup%2FCreate_ME" \
-    -d '{ "managed_device_name": "Cisco WSA",  "model_id": "95", "manufacturer_id": "1",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa", }'
+    -d '{ "managed_device_name": "Cisco WSA",  "model_id": "95", "manufacturer_id": "1",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa"}'
 
 echo "#### Cisco Catalyste IOS"
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/orchestration/service/execute/status/$CUSTID/?serviceName=Process/SelfDemoSetup/SelfDemoSetup&processName=Process%2FSelfDemoSetup%2FCreate_ME" \
-    -d '{ "managed_device_name": "Cisco Catalyst IOS",  "model_id": "104", "manufacturer_id": "1",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa", }'
+    -d '{ "managed_device_name": "Cisco Catalyst IOS",  "model_id": "104", "manufacturer_id": "1",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa"}'
 
 echo "#### SDN Cisco APIC"
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer "$TOKEN -XPOST "http://127.0.0.1/ubi-api-rest/orchestration/service/execute/status/$CUSTID/?serviceName=Process/SelfDemoSetup/SelfDemoSetup&processName=Process%2FSelfDemoSetup%2FCreate_ME" \
-    -d '{ "managed_device_name": "SDN Cisco APIC",  "model_id": "133", "manufacturer_id": "27",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa", }'
+    -d '{ "managed_device_name": "SDN Cisco APIC",  "model_id": "133", "manufacturer_id": "27",  "password": "ubiqube",  "snmpCommunity": "ubiqube",  "password_admin": "aaaa",  "managementInterface": "eth0",  "device_ip_address": "192.168.1.1",  "login": "msa"}'
 
 
 echo
