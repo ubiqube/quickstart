c13061f2e5a6fdf2ca0bb4c25bdb622dc8dfd850
Antoine Brun <antoine.brun@openmsa.co>
2020-07-06 18:24:10 +0200

    add juniper SRX

--- lab/api/resources/install_libraries.sh
+++ lab/api/resources/install_libraries.sh
@@ -153,6 +153,7 @@ install_adapter paloalto_generic
 install_adapter paloalto_vsys
 
 install_adapter netconf_generic
+install_adapter juniper_srx
 
 #install_adapter stormshield 
 #install_adapter a10_thunder 
