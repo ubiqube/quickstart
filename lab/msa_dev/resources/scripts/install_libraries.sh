556eb270dae5a4e4d53f6b88e325d4ac7211ec13
antoine-brun <antoine.brun@gmail.com>
2020-08-17 16:44:36 +0200

    merge Workflow branch 2.1.0GA to master

--- lab/msa_dev/resources/scripts/install_libraries.sh
+++ lab/msa_dev/resources/scripts/install_libraries.sh
@@ -19,7 +19,7 @@ update_github_repo() {
         git stash pop;
         # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         ### TODO REMOVE BEFORE PR MERGE
-        git checkout 2.1.0GA;
+        #git checkout 2.1.0GA;
         # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         git pull;
     else 
