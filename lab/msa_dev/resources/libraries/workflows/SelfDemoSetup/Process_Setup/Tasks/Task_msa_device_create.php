bd7bd935ca7cae8bd4ff05817619bc59a39f0bc9
root <antoine.brun@ubiqube.com>
2021-01-13 10:02:28 +0000

    create ME with syslog disabled

--- lab/msa_dev/resources/libraries/workflows/SelfDemoSetup/Process_Setup/Tasks/Task_msa_device_create.php
+++ lab/msa_dev/resources/libraries/workflows/SelfDemoSetup/Process_Setup/Tasks/Task_msa_device_create.php
@@ -37,8 +37,8 @@ $password_admin = $context['password_admin'];
 $management_address = $context['device_ip_address'];
 $device_external_reference = "";
 $log_enabled = "true";
-$log_more_enabled = "true";
-$mail_alerting = "true";
+$log_more_enabled = "false";
+$mail_alerting = "false";
 $reporting = "false";
 $snmp_community = $context['snmpCommunity'];
 $managementInterface = $context['managementInterface'];
@@ -48,8 +48,8 @@ $management_port = $context['management_port'];
 $response = _device_create($customer_db_id, $device_name, $manufacturer_id,
 							$model_id, $login, $password, $password_admin, 
 							$management_address, $device_external_reference, 
-							$log_enabled = "true", $log_more_enabled = "true",
-							$mail_alerting = "true", $reporting = "true", $snmp_community, 
+							$log_enabled = "true", $log_more_enabled = "false",
+							$mail_alerting = "false", $reporting = "false", $snmp_community, 
 							$managementInterface = "", $hostname, $management_port);
 
 $response = json_decode($response, true);
