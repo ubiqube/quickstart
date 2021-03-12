9086ff9685e49b66e1a94b1657b35f694e7b9418
ArnaudFosse <34027417+ArnaudFosse@users.noreply.github.com>
2021-03-12 21:31:46 +0100

    Update install.sh
    Disable clean_old_topology_instances.sh call, as missing right

--- scripts/install.sh
+++ scripts/install.sh
@@ -66,7 +66,8 @@ standaloneUpgrade(){
 		docker-compose exec msa_dev /usr/bin/migrate_bpmn.sh -r
 
 		echo "Removing old instances of topology"
-		docker-compose exec msa_dev /usr/bin/clean_old_topology_instances.sh
+		# Disable are execution right is missing on this file. Need to generate again a DEV image
+		#docker-compose exec msa_dev /usr/bin/clean_old_topology_instances.sh
 	fi
 
 	echo "Elasticsearch settings & mappings update"
