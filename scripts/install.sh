86322c371524320b0c8f7bf4b0be510516638f21
antoine-brun <antoine.brun@gmail.com>
2022-01-24 11:20:32 +0100

    fix service names

--- scripts/install.sh
+++ scripts/install.sh
@@ -44,32 +44,32 @@ standaloneInstall(){
         docker-compose -f docker-compose.yml -f lab/mano/docker-compose.mano.yml up -d --build
     fi
 
-    docker-compose exec -T msa_dev rm -rf /opt/fmc_repository/Process/Reference
+    docker-compose exec -T msa-dev rm -rf /opt/fmc_repository/Process/Reference
 
-    docker-compose exec -T msa_dev /usr/bin/install_libraries.sh $(getLibOptions)
+    docker-compose exec -T msa-dev /usr/bin/install_libraries.sh $(getLibOptions)
 
-    docker-compose restart msa_api
-    docker-compose restart msa_sms
-    docker-compose restart msa_alarm
+    docker-compose restart msa-api
+    docker-compose restart msa-sms
+    docker-compose restart msa-alarm
 
     echo "Starting crond on API container msa_api"
-    docker-compose exec -T -u root msa_api crond
+    docker-compose exec -T -u root msa-api crond
     echo "Done"
 
     if [ $fresh_setup = false ] ; then
         echo "Remove AI ML database. Required on upgrades from 2.4"
-        docker-compose exec -T -u root msa_ai_ml /bin/bash -c 'rm /msa_proj/database/db.sqlite3'
-        docker-compose restart msa_ai_ml
+        docker-compose exec -T -u root msa-ai-ml /bin/bash -c 'rm /msa_proj/database/db.sqlite3'
+        docker-compose restart msa-ai-ml
 
         echo "Elasticsearch : .kibana_1 index regeneration"
-        docker-compose exec -T -u root -w /home/install/scripts/ msa_es bash -c './kibana_index_update.sh'
+        docker-compose exec -T -u root -w /home/install/scripts/ msa-es bash -c './kibana_index_update.sh'
         echo "Done"
     fi
 
 
     echo "Kibana configs & dashboard templates update"
     waitUpKibana 127.0.0.1
-    docker-compose exec -T -u root -w /home/install/scripts msa_kibana bash -c 'php install_default_template_dash_and_visu.php'
+    docker-compose exec -T -u root -w /home/install/scripts msa-kibana bash -c 'php install_default_template_dash_and_visu.php'
     echo "Done"
 
     upgrade_done
@@ -143,7 +143,7 @@ upgrade_done(){
 
 miniLabCreation(){
     if [ $ha_setup = false ] ; then
-        docker-compose exec -T msa_dev /usr/bin/create_mini_lab.sh
+        docker-compose exec -T msa-dev /usr/bin/create_mini_lab.sh
     else
         ha_dev_node_ip=$(getHaNodeIp msa_dev)
         ha_dev_container_ref=$(getHaContainerReference msa_dev)
