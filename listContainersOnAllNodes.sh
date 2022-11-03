f1e034bb395781e456f6e2efcaf3035c683fece6
abr-ubiqube <antoine.brun@ubiqube.com>
2022-11-03 21:11:42 +0800

    add swarm worker

--- listContainersOnAllNodes.sh
+++ listContainersOnAllNodes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
- for node in 127.0.0.1 mano2 mano3 mano4;do echo "#################### $node #######################";ssh $node "docker ps";echo;done
+ for node in 127.0.0.1 mano1 mano2 mano3 mano4;do echo "#################### $node #######################";ssh $node "docker ps";echo;done
 
