1306119281a3d6adde7df8081d805057fd1f2a26
ydu <ydu@ubiqube.com>
2022-11-28 12:14:40 +0100

    improve inspectNodes.sh

--- scripts/inspectNodes.sh
+++ scripts/inspectNodes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NODES=$(docker node ls --format '{{.Hostname}}')
 #NODES=$(docker node ls -f "role=worker" -f "node.label=worker=app" --format '{{.Hostname}}')
-docker node inspect --format '{{printf "%-32s%-16s%-16s%s" .Description.Hostname .Status.Addr .Status.State .Spec.Labels}}' $NODES
+docker node inspect --format '{{printf "%-32s%-16s%-16s" .Description.Hostname .Status.Addr .Status.State}} {{range $key, $val := .Spec.Labels}}{{printf "%s:%s " $key $val }}{{end}}' $NODES
