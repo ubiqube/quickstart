57cb8203c22ddd4ca03ed38dc84502fb4db66fb8
Antoine Brun <antoine.brun@gmail.com>
2023-03-27 18:13:24 +0200

    update docker-monitor service

--- scripts/docker-monitor.sh
+++ scripts/docker-monitor.sh
@@ -12,11 +12,11 @@ monitor_swarm_docker_events()
 {
   DOCKER_SERVICE_SMS="${DOCKER_STACK_NAME}_${SMS}"
 
-  docker events --filter 'scope=local' --format 'Type={{.Type}}  Status={{.Status}}  From={{.From}}  ID={{.ID}} Action={{.Action}}' | while read event
+  docker system events --format 'type={{.Type}}  status={{.Status}}  from={{.From}}  ID={{.ID}} action={{.Action}} scope={{.Scope}}' | while read event
   do
-      d=$(date +"$DATE_FORMAT")
-      echo "$d  send syslog for event $event"
-      echo $event | nc -w1 -u $SYSLOG_SERVER 514
+    d=$(date +"$DATE_FORMAT")
+    echo "$d  send syslog for event $event"
+    echo "<14> $event" | nc -v -u -w1 $SYSLOG_SERVER 514
   done
 }
 
@@ -25,11 +25,11 @@ monitor_docker_events()
   local state="healthy"
   DOCKER_CONTAINER_SMS=$(docker ps --format {{.Names}} -f name=${SMS})
 
-  docker events --filter 'scope=local' --format 'Type={{.Type}}  Status={{.Status}}  From={{.From}}  ID={{.ID}} Action={{.Action}}' | while read event
+  docker events --format 'Type={{.Type}}  Status={{.Status}}  From={{.From}}  ID={{.ID}} Action={{.Action}} scope={{.Scope}}' | while read event
   do
     d=$(date +"$DATE_FORMAT")
     echo "$d  send syslog for event $event"
-    echo $event | nc -w1 -u $SYSLOG_SERVER 514
+    echo "<14> $event" | nc -v -u -w1 $SYSLOG_SERVER 514
   done
 }
 
