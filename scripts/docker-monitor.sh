e27a6acdf4621a8f4f51d787d1640811f5cb59b3
Antoine Brun <antoine.brun@gmail.com>
2023-03-31 16:46:53 +0200

    update docker event CLI cmd

--- scripts/docker-monitor.sh
+++ scripts/docker-monitor.sh
@@ -18,7 +18,7 @@ JQ="/usr/bin/jq -M -r"
 # Monitor Docker events
 monitor_swarm_docker_events()
 {
-  docker system events --format 'type={{.Type}}  status={{.Status}}  from={{.From}}  ID={{.ID}} action={{.Action}} scope={{.Scope}}' | while read event
+  docker system events --filter 'scope=swarm' --format 'type={{.Type}}  status={{.Status}}  from={{.From}}  ID={{.Actor.ID}} action={{.Action}} scope={{.Scope}}  {{range $key, $val := .Actor.Attributes}}{{printf "%s=%s " $key $val }}{{end}}' | while read event
   do
     d=$(date +"$DATE_FORMAT")
     echo "$d  send syslog for event $event"
@@ -28,7 +28,7 @@ monitor_swarm_docker_events()
 
 monitor_docker_events()
 {
-  docker system events --format 'type={{.Type}}  status={{.Status}}  from={{.From}}  ID={{.ID}} action={{.Action}} scope={{.Scope}}' | while read event
+  docker system events --filter 'scope=local' --format 'type={{.Type}}  status={{.Status}}  from={{.From}}  ID={{.ID}} action={{.Action}} scope={{.Scope}}' | grep -v "container exec_" | while read event
   do
     d=$(date +"$DATE_FORMAT")
     echo "$d  send syslog for event $event"
