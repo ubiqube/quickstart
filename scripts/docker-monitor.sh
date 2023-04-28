ae0bbc27537dc60d29ce5c1d29842583e9a5aa26
Antoine Brun <antoine.brun@openmsa.co>
2023-04-28 13:08:49 +0200

    2.8.7 (#194)
    * Now one es-configuration shortcut defines ES_CREDENTIALS and ES_SERVERS, and is used in all containers where these variables are needed.
    
    * Missing quote added.
    
    * update 2.8.7b45
    
    * update 2.8.7b46
    
    * update 2.8.7b48
    
    * update 2.8.7b51
    
    * update 2.8.7b51
    
    * update 2.8.7b58
    
    * update 2.8.7b67
    
    * remove empty line
    
    * update 2.8.7b68
    
    * add rsyslog volume
    
    * update 2.8.7
    
    * update 2.8.7b71
    
    * add nat exception for 6514
    
    * fix nat exception to TCP for 6514
    
    * fix delete_nat_exception / update msg
    
    * manage multiple stacks in listServiceNodes.sh
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update 2.8.7
    
    * Docker event monitoring (#191)
    
    * initial commit
    
    * change docker event param
    
    * use netcat to send syslog
    
    * update 2.8.7
    
    ---------
    
    Co-authored-by: Antoine Brun <antoine.brun@gmail.com>
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update 2.8.7
    
    * fix for syslog generation
    
    * update 2.8.7
    
    * updater docker-monitor
    
    * add include file for SYSLog server IP
    
    * fix conflict
    
    * update 2.8.7
    
    * add rsyslog env
    
    * update 2.8.7
    
    * update docker event CLI cmd
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update 2.8.7
    
    * MSA-12035: split /opt/sms/spool/parser to separate logs to index and errors
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update 2.8.7
    
    * update for 2.8.7 version
    
    * update for 2.8.7 version
    
    * update images 2.8.7
    
    * update images 2.8.7
    
    * update images .2.8.7
    
    * update .2.8.7 build 176
    
    * update 2.8.7 build 176
    
    * update 2.8.7 build 176
    
    * Updated docker stack command
    
    ---------
    
    Co-authored-by: yguubiqube <ygu@ubiqube.com>
    Co-authored-by: Yves Guerte <66126570+yguubiqube@users.noreply.github.com>
    Co-authored-by: Antoine Brun <antoine.brun@gmail.com>
    Co-authored-by: ydu <ydu@ubiqube.com>
    Co-authored-by: root <root@ip-172-31-0-50.eu-west-2.compute.internal>
    Co-authored-by: QA SPE <spe@ubiqube.com>
    Co-authored-by: gurupr77 <gpr@ubiqube.com>

---
+++ scripts/docker-monitor.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -x
+
+APPDIR=$(dirname $0)
+source $APPDIR/docker-monitor.inc
+HOST=$(hostname)
+if [ -z ${SYSLOG_SERVER} ]; then
+    echo "WARNING: SYSLOG_SERVER not set, update docker-monitor.inc"
+    exit 1
+fi
+
+SMS="msa-sms"
+DATE_FORMAT="%Y-%m-%d %H:%M:%S"
+LOGFILE=/var/log/docker-monitor.log
+# jq no color and raw output
+JQ="/usr/bin/jq -M -r"
+
+# Monitor Docker events
+monitor_swarm_docker_events()
+{
+  docker system events --filter 'scope=swarm' --format 'type={{.Type}}  status={{.Status}}  from={{.From}}  ID={{.Actor.ID}} action={{.Action}} scope={{.Scope}}  {{range $key, $val := .Actor.Attributes}}{{printf "%s=%s " $key $val }}{{end}}' | while read event
+  do
+    d=$(date +"$DATE_FORMAT")
+    echo "$d  send syslog for event $event"
+    echo "<14> $event" | nc -v -u -w1 $SYSLOG_SERVER 514
+  done
+}
+
+monitor_docker_events()
+{
+  docker system events --filter 'scope=local' --format 'type={{.Type}}  status={{.Status}}  from={{.From}}  ID={{.ID}} action={{.Action}} scope={{.Scope}}' | grep -v "container exec_" | while read event
+  do
+    d=$(date +"$DATE_FORMAT")
+    echo "$d  send syslog for event $event"
+    echo "<14> $event" | nc -v -u -w1 $SYSLOG_SERVER 514
+  done
+}
+
+# USER does not exist if the script is executed as a service (systemd)
+[ -z "$USER" ] && export USER=root
+
+# Get the swarm name if any
+DOCKER_STACK_NAME=''
+STACK_NAMES=$(docker stack ls --format {{.Name}} 2>/dev/null)
+if [ $? -eq 0 ]; then
+  for s in $STACK_NAMES; do
+    docker stack ps "$s" --filter "name=${s}_${SMS}" -q >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+      # this is the right stack
+      DOCKER_STACK_NAME="$s"
+      break
+    fi
+  done
+fi
+
+exec >>${LOGFILE} 2>&1
+
+if [ -n "$DOCKER_STACK_NAME" ]; then
+  monitor_swarm_docker_events
+else
+  monitor_docker_events
+fi
