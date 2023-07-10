eee88229284c3344f282deabc47c581c8249eb64
abr-ubiqube <antoine.brun@ubiqube.com>
2023-07-10 16:57:58 +0800

    add mano8

--- listContainersOnAllNodes.sh
+++ listContainersOnAllNodes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
- for node in 127.0.0.1 mano1 mano2 mano3 mano4 mano5 mano6 mano7;do echo "#################### $node #######################";ssh $node "docker ps";echo;done
+ for node in 127.0.0.1 mano1 mano2 mano3 mano4 mano5 mano6 mano7 mano8;do echo "#################### $node #######################";ssh $node "docker ps";echo;done
 
