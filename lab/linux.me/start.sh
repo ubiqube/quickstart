791132c6e6528076cb1fb2b07436b9892dcad497
Olivier Vignaud <ovi@ubiqube.com>
2023-08-07 16:51:11 +0200

    Remove linuxme and msa-dev.

--- lab/linux.me/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -x
-sudo iptables -I INPUT -p udp -m udp --dport 161 -j ACCEPT
-sudo iptables -I INPUT -p udp -m udp --dport 162 -j ACCEPT
-sudo iptables-save > /etc/sysconfig/iptables
-
-
